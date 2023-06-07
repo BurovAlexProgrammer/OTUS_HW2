d4b18ca53a1335310661a9407720dbf0e035fd90
Aleksei Burov <burovalexprogrammer@gmail.com>
2023-06-07 21:28:02 +0200

    GameServicesInstaller.cs

---
+++ Assets/Scripts/GameContext/GameServicesInstaller.cs
@@ -0,0 +1,25 @@
+﻿using Service;
+using UnityEngine;
+
+namespace GameContext
+{
+    public class GameServicesInstaller : MonoBehaviour
+    {
+        private ServiceBase[] _services;
+
+        private void Awake()
+        {
+            _services = gameObject.GetComponentsInChildren<ServiceBase>();
+
+            InstallServices();
+        }
+
+        private void InstallServices()
+        {
+            foreach (var service in _services)
+            {
+                ServiceLocator.Add(service);
+            }
+        }
+    }
+}
